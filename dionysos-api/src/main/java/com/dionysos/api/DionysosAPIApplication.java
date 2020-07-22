2726d79cbf405c9ce719008a19064e49caefe235
kor-Chipmunk <rhj4862@gmail.com>
2020-07-22 17:34:04 +0900

    Application 클래스 생성

---
+++ dionysos-api/src/main/java/com/dionysos/api/DionysosAPIApplication.java
@@ -0,0 +1,11 @@
+package com.dionysos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DionysosAPIApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DionysosAPIApplication.class, args);
+    }
+}
---
+++ dionysos-api/src/test/java/com/dionysos/api/DionysosAPIApplicationTest.java
@@ -0,0 +1,13 @@
+package com.dionysos.api;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DionysosAPIApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
