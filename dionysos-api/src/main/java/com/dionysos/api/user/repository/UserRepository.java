fbf377b28d389b11c957daa0313cd5e2ddd237ba
kor-Chipmunk <rhj4862@gmail.com>
2020-07-24 04:29:52 +0900

    feat: #1. 회원가입, 로그인 JWT 연동

--- dionysos-api/src/main/java/com/dionysos/api/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dionysos.api.user.repository;
-
-import com.dionysos.api.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
