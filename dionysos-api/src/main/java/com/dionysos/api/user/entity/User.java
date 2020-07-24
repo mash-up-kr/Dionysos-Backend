6d8e3e3211917a8d4cb7a495cd85aeecdd3c9c68
kor-Chipmunk <rhj4862@gmail.com>
2020-07-25 04:06:46 +0900

    feat: User Entity 클래스 필드 수정과 빌더 추가

--- dionysos-api/src/main/java/com/dionysos/api/user/entity/User.java
+++ dionysos-api/src/main/java/com/dionysos/api/user/entity/User.java
@@ -2,10 +2,13 @@ package com.dionysos.api.user.entity;
 
 import com.dionysos.api.user.dto.RequestSignUpDto;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -14,9 +17,18 @@ import javax.persistence.Id;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String uid;
 
     private String nickname;
+
+    @Builder
+    public User(String uid, String nickname) {
+        this.uid = uid;
+        this.nickname = nickname;
+    }
 
     private User(RequestSignUpDto requestSignUpDto) {
         this.uid = requestSignUpDto.getUid();
