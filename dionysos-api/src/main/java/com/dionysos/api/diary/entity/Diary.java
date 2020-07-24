2449045d56a9a63601dd90d7eee77147348f3805
kor-Chipmunk <rhj4862@gmail.com>
2020-07-25 04:07:54 +0900

    feat: Diary Entity 클래스 빌더 생성자 추가

--- dionysos-api/src/main/java/com/dionysos/api/diary/entity/Diary.java
+++ dionysos-api/src/main/java/com/dionysos/api/diary/entity/Diary.java
@@ -1,6 +1,7 @@
 package com.dionysos.api.diary.entity;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,4 +20,10 @@ public class Diary {
 
     private String imageUrl;
     private String content;
+
+    @Builder
+    public Diary(String imageUrl, String content) {
+        this.imageUrl = imageUrl;
+        this.content = content;
+    }
 }
