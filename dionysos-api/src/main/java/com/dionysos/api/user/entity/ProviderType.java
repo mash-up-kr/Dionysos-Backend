ceb00fb300a618600d51c3d22f0ca1ec2d431fcb
kor-Chipmunk <rhj4862@gmail.com>
2020-07-25 16:21:28 +0900

    feat: User Entity provider 필드 추가

---
+++ dionysos-api/src/main/java/com/dionysos/api/user/entity/ProviderType.java
@@ -0,0 +1,9 @@
+package com.dionysos.api.user.entity;
+
+public enum ProviderType {
+    KAKAO,
+    NAVER,
+    FACEBOOK,
+    GOOGLE,
+    APPLE
+}
--- dionysos-api/src/main/java/com/dionysos/api/user/entity/User.java
+++ dionysos-api/src/main/java/com/dionysos/api/user/entity/User.java
@@ -2,10 +2,7 @@ package com.dionysos.api.user.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -20,6 +17,9 @@ public class User {
     private String uid;
 
     private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private ProviderType provider;
 
     @Builder
     public User(String uid, String nickname) {
