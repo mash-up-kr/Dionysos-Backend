bf3a820ffbc5836e844f62b62067ad988599ccfa
kor-Chipmunk <rhj4862@gmail.com>
2020-07-27 20:18:29 +0900

    refactor: #25. 누적 시간 기록 컨트롤러 생성자 주입으로 변경

--- dionysos-api/src/main/java/com/dionysos/api/timehistory/controller/TimeHistoryController.java
+++ dionysos-api/src/main/java/com/dionysos/api/timehistory/controller/TimeHistoryController.java
@@ -2,7 +2,7 @@ package com.dionysos.api.timehistory.controller;
 
 import com.dionysos.api.timehistory.dto.RequestTimeHistoryDto;
 import com.dionysos.api.timehistory.service.TimeHistoryService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,11 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/time-history")
 public class TimeHistoryController {
 
-    @Autowired
-    private TimeHistoryService timeHistoryService;
+    private final TimeHistoryService timeHistoryService;
 
     @PostMapping("")
     public ResponseEntity create(@RequestBody RequestTimeHistoryDto requestTimeHistoryDto) {
