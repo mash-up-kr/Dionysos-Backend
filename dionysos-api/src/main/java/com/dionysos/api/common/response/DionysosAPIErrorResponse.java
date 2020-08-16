ef98fa9d7e7105396fb7efc9206eb7b5ef3f1264
kor-Chipmunk <rhj4862@gmail.com>
2020-08-16 17:26:56 +0900

    refactor: #49. 오류 리스트 삭제

--- dionysos-api/src/main/java/com/dionysos/api/common/response/DionysosAPIErrorResponse.java
+++ dionysos-api/src/main/java/com/dionysos/api/common/response/DionysosAPIErrorResponse.java
@@ -5,22 +5,16 @@ import com.dionysos.api.common.response.code.DionysosAPIErrorCode;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
 public class DionysosAPIErrorResponse<T extends ErrorModel> {
 
     private int code;
     private String message;
-    private List<T> errors;
 
     @Builder
-    private DionysosAPIErrorResponse(DionysosAPIErrorCode errorCode,
-                                     List<T> errors
-    ) {
+    private DionysosAPIErrorResponse(DionysosAPIErrorCode errorCode) {
         this.code = errorCode.getCode();
         this.message = errorCode.getDescription();
-        this.errors = errors;
     }
 
 }
