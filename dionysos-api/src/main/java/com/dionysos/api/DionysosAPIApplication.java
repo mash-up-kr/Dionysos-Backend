488504163709cc6db176c0cb97f1a1e9c8bc43d3
kor-Chipmunk <rhj4862@gmail.com>
2020-09-10 04:42:58 +0900

    refactor: #5. Application EnableCaching 어노테이션 삭제

--- dionysos-api/src/main/java/com/dionysos/api/DionysosAPIApplication.java
+++ dionysos-api/src/main/java/com/dionysos/api/DionysosAPIApplication.java
@@ -2,9 +2,7 @@ package com.dionysos.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
-@EnableCaching
 @SpringBootApplication
 public class DionysosAPIApplication {
     public static void main(String[] args) {
