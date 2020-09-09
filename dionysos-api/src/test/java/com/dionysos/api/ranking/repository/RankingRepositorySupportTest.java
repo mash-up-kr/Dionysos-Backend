b0c5fb45e5dc179122b579eb4cda1ff6477d0220
kor-Chipmunk <rhj4862@gmail.com>
2020-09-10 04:46:57 +0900

    test: #5. 랭킹 서비스 테스트 파일 생성

---
+++ dionysos-api/src/test/java/com/dionysos/api/ranking/repository/RankingRepositorySupportTest.java
@@ -0,0 +1,29 @@
+package com.dionysos.api.ranking.repository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RankingRepositorySupportTest {
+
+    @Test
+    public void 일일_랭킹_테스트() {
+
+    }
+
+    @Test
+    public void 주별_랭킹_테스트() {
+
+    }
+
+    @Test
+    public void 월별_랭킹_테스트() {
+
+    }
+
+}
