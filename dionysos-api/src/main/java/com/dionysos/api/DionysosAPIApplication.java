b4bd7d7ff4285b72b382bd7f165fd127e10edc69
ParkMiHyun <mihyunpark24@gmail.com>
2020-09-10 03:29:34 +0900

    feat:#5 Redis, EmbeddedRedis config, 캐시 정적 변수bean 추가

--- dionysos-api/src/main/java/com/dionysos/api/DionysosAPIApplication.java
+++ dionysos-api/src/main/java/com/dionysos/api/DionysosAPIApplication.java
@@ -2,7 +2,9 @@ package com.dionysos.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class DionysosAPIApplication {
     public static void main(String[] args) {
